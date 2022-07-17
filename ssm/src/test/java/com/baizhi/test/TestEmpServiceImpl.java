1c59e0b19346cfe189715ead814547fb2f0efc76
Scy_c <2402265378@qq.com>
2022-07-17 10:06:58 +0800

    1

--- ssm/src/test/java/com/baizhi/test/TestEmpServiceImpl.java
+++ ssm/src/test/java/com/baizhi/test/TestEmpServiceImpl.java
@@ -14,7 +14,7 @@ public class TestEmpServiceImpl {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         EmpService bean = (EmpService) context.getBean("empServiceImpl");
-       /* bean.save(new Emp(2 ,"xa io",new Date(),23.23));*/
+       /* bean.save(new Emp(2 ,"xa io",new Date(),23.23`1));*/
        bean.findAll().forEach(emp -> {System.out.println(emp);});
 
     }
